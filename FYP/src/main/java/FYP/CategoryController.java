b9b2ed51974544b777c006bb9c6f1d0c53c3e81b
DeckEcho <p.arveer.singh0408@gmail.com>
2023-08-07 13:40:04 +0800

    Parveer

--- FYP/src/main/java/FYP/CategoryController.java
+++ FYP/src/main/java/FYP/CategoryController.java
@@ -13,6 +13,7 @@
 package FYP;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,65 +28,70 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class CategoryController {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ItemRepository itemRepository;
-	
+
 	@GetMapping("/categories")
 	public String viewCategories(Model model) {
-		
+
 		List<Category> listCategories = categoryRepository.findAll();
-		
+
 		model.addAttribute("listCategories", listCategories);
 		return "view_categories";
 	}
-	
+
 	@GetMapping("/categories/add")
 	public String addCategory(Model model) {
 		model.addAttribute("category", new Category());
 		return "add_category";
 	}
-	
+
 	@PostMapping("/categories/save")
 	public String saveCategory(Category category) {
 		categoryRepository.save(category);
 		return "redirect:/categories";
 	}
-	
+
 	@GetMapping("/categories/edit/{id}")
 	public String editCategory(@PathVariable("id") Long id, Model model) {
 		Category category = categoryRepository.getById(id);
 		model.addAttribute("category", category);
-		
+
 		return "edit_category";
 	}
-	
+
 	@PostMapping("/categories/edit/{id}")
 	public String saveUpdatedCategory(@PathVariable("id") Long id, Category category) {
 		categoryRepository.save(category);
 		return "redirect:/categories";
-	
+
 	}
 
 	@GetMapping("/categories/delete/{id}")
-    public String deleteCategory(@PathVariable("id") Long id) {
-  	  
-  	  categoryRepository.deleteById(id);
-  	  
-  	  return "redirect:/categories";
-    }
-	
+	public String deleteCategory(@PathVariable("id") Long id) {
+
+		categoryRepository.deleteById(id);
+
+		return "redirect:/categories";
+	}
+
 	@GetMapping("/categories/{categoryId}")
 	public String viewItemsByCategory(@PathVariable("categoryId") Long categoryId, Model model) {
-	    Category category = categoryRepository.findById(categoryId)
-	        .orElseThrow(() -> new IllegalArgumentException("Invalid category ID: " + categoryId));
-	    List<Item> items = itemRepository.findByCategoryId(categoryId);
-	    model.addAttribute("category", category);
-	    model.addAttribute("items", items);
-	    return "view_items_by_category";
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid category ID: " + categoryId));
+		List<Item> items = itemRepository.findByCategoryId(categoryId);
+		model.addAttribute("category", category);
+		model.addAttribute("items", filterItemsWithZeroQuantity(items));
+		return "view_items_by_category";
+
+	}
+
+	private List<Item> filterItemsWithZeroQuantity(List<Item> itemList) {
+		return itemList.stream().filter(item -> item.getQuantity() > 0).collect(Collectors.toList());
 	}
 
 }
